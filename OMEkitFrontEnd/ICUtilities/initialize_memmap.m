f094db3f25b3c794589dd918a9a9c56d7c96f6b4
yalexand <yalexand@ic.ac.uk>
2015-03-02 15:52:21 +0000

    ic_OPTtools - forgotten changes..

--- OMEkitFrontEnd/ICUtilities/initialize_memmap.m
+++ OMEkitFrontEnd/ICUtilities/initialize_memmap.m
@@ -1,4 +1,4 @@
-function [ mapfile_name,memmap ] = initialize_memmap( size_arr, var_name, datatype, varargin )
+function [ mapfile_name,memmap ] = initialize_memmap( size_arr, repeats, var_name, datatype, varargin )
 
             ip = inputParser;
             ip.addOptional('ini_data', [], @isnumeric);
@@ -6,10 +6,10 @@ function [ mapfile_name,memmap ] = initialize_memmap( size_arr, var_name, dataty
 
             mapfile_name = global_tempname;                        
             mapfile = fopen(mapfile_name,'w');
-            ini_data = zeros(1,prod(size_arr),datatype);
+            ini_data = zeros(1,prod(size_arr)*repeats,datatype);
             fwrite(mapfile,ini_data,datatype);
             fclose(mapfile);
-            memmap = memmapfile(mapfile_name,'Writable',true,'Repeat',1,'Format',{datatype, size_arr, var_name},'Offset',0);
+            memmap = memmapfile(mapfile_name,'Writable',true,'Repeat',repeats,'Format',{datatype, size_arr, var_name},'Offset',0);
             
             if ~isempty(ip.Results.ini_data)
                 memmap.Data.(var_name) = ip.Results.ini_data;
